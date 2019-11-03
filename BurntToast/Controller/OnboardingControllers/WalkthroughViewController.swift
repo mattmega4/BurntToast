190a37111b95865cf30d3aebaf21552020322812
mattmega4 <mattmega4@gmail.com>
2019-11-03 14:52:27 -0800

    fixes outlets

--- BurntToast/Controller/OnboardingControllers/WalkthroughViewController.swift
+++ BurntToast/Controller/OnboardingControllers/WalkthroughViewController.swift
@@ -16,20 +16,61 @@ class WalkthroughViewController: UIViewController {
   @IBOutlet weak var nextButton: UIButton!
   @IBOutlet weak var skipButton: UIButton!
 
+  //MARK: - Properties
+
+  var walkthroughPageViewController: WalkthroughPageViewController?
   
   override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-  @IBAction func NextButtonTapped(_ sender: UIButton) {
-    
+    // Do any additional setup after loading the view.
   }
-  
+
+
+  @IBAction func nextButtonTapped(_ sender: UIButton) {
+    if let index = walkthroughPageViewController?.currentIndex {
+      switch index {
+      case 0...1:
+        walkthroughPageViewController?.forwardPage()
+      case 2:
+        dismiss(animated: true, completion: nil)
+      default:
+        break
+      }
+    }
+    updateUI()
+  }
+
+
+  func updateUI() {
+    if let index = walkthroughPageViewController?.currentIndex {
+      switch index {
+      case 0...1:
+        nextButton.setTitle("NEXT", for: .normal)
+      case 2:
+        nextButton.setTitle("Create Account", for: .normal)
+      default:
+        break
+      }
+      pageControl.currentPage = index
+    }
+
+  }
+
+
   @IBAction func skipButtonTapped(_ sender: UIButton) {
     dismiss(animated: true, completion: nil)
+  }
+
+
+
+  //MARK: - Navigation
+
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let destination = segue.destination
+    if let pageViewController = destination as? WalkthroughPageViewController {
+      walkthroughPageViewController = pageViewController
+    }
   }
 
 }
